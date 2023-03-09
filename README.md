dbd9cfc9275e53542d50f965a444f569c3432cf9
Henrique Dante de Almeida <henrique.almeida@linea.org.br>
2023-03-09 18:10:26 -0300

    Initial implementation of rail-estimate program
    The script allows calculating PZ tables with a single command. This
    program was missing in the RAIL source distribution and it would make
    sense if it were eventually submitted upstream to the RAIL repository.
    
    Script help message with usage:
    
    Generate photo-z estimate tables from raw data.
    
    Usage:
        rail-estimate [--algorithm=<name>] [--] <input> <output>
        rail-estimate -h | --help
        rail-estimate --version
    
    Options:
        -h --help   Show help text.
        --version   Show version.
        --algorithm=<name>  Select estimation algorithm [default: fzboost].
    
    Available algorithms: bpz, fzboost

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Pipeline to compute photo-zs using public codes for large volumes of data in the Brazilian's LSST IDAC infrastructure. 
 
 This repository contains software developed and delivered as part of the in-kind contribution program BRA-LIN, from LIneA to the Rubin Observatory's LSST. An overview of this and other contributions is available [here](https://linea-it.github.io/pz-lsst-inkind-doc/).  
+
+## rail-estimate
+The `rail-estimate` program is the main standalone script used to
+compute the PZ tables. The script accepts an input file containing raw
+data and outputs the estimated photo-z values as a probability
+distribution.
---
+++ rail_scripts/rail_scripts/__init__.py
@@ -0,0 +1,51 @@
+from os.path import isfile, join
+from sys import stderr
+
+from xdg.BaseDirectory import load_data_paths
+
+VERSION_TEXT = 'rail_scripts 0.1.0'
+PROJECT_NAME = 'rail_scripts'
+ESTIMATOR_CONFIGURATION_TEMPLATE = 'estimator_%s.pkl'
+
+def abort():
+    raise SystemExit(1)
+
+def error(msg, **kwargs):
+    print(msg, file=stderr, **kwargs)
+
+info = debug = warning = error
+
+def find_data_file(file_name):
+    if isfile(file_name):
+        return file_name
+
+    for n in load_data_paths(PROJECT_NAME, file_name):
+        if isfile(n):
+            return n
+
+    # Non existent file just returns the original file name, so that it
+    # can be tracked later.
+    return file_name
+
+def find_estimator_configuration(model_name):
+    model_file = ESTIMATOR_CONFIGURATION_TEMPLATE % model_name
+
+    return find_data_file(model_file)
+
+def create_column_mapping_dict(column_template, column_template_error,
+                               def_maglimits):
+    bands = ('u', 'g', 'r', 'i', 'z', 'y')
+
+    band_names = [column_template.format(band=band) for band in bands]
+    band_err_names = [column_template_error.format(band=band) for band in bands]
+    ref_band = column_template.format(band='i')
+
+    mag_limits = {name: def_maglimits['mag_%s_lsst' % band] for (band, name) in
+                  zip(bands, band_names)}
+
+    return {
+            'bands': band_names, 'band_names': band_names,
+            'err_bands': band_err_names, 'band_err_names': band_err_names,
+            'ref_band': ref_band, 'prior_band': ref_band,
+            'mag_limits': mag_limits,
+    }
