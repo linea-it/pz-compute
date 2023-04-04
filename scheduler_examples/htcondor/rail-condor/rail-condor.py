a86cb7683be4d2698616dbcfbc69b86066c97249
Henrique Dante de Almeida <henrique.almeida@linea.org.br>
2023-04-04 11:16:11 -0300

    Add condor scripts for processing files with heterogeneous names
    The example scripts, instead of assuming files numbered in order,
    inspect the contents of the input directory and create apropriate
    command-line arguments for processing them. The script supports queueing
    multiple files per condor task. The input directory is inspected
    recursivelly and the outputs will have names matching the inputs.

---
+++ scheduler_examples/htcondor/rail-condor/rail-condor.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from contextlib import contextmanager
+from datetime import datetime
+from os import environ, makedirs
+from os.path import dirname
+from shutil import which
+from subprocess import run
+from sys import argv, stderr
+from time import sleep
+
+@contextmanager
+def now(msg):
+    print ('%s: Starting: %s' % (datetime.now(), msg), file=stderr, flush=True)
+    yield
+    print ('%s: Finished: %s' % (datetime.now(), msg), file=stderr, flush=True)
+
+def pairwise(iterable):
+    a = iter(iterable)
+    return zip(a, a)
+
+def main():
+    with now('rail-condor: slot=%s' % environ.get('_CONDOR_SLOT')):
+        with now('sleep for 30 seconds (condor warm-up)'):
+            sleep(30)
+
+        procid = int(argv[1])
+        paths = argv[2:]
+
+        assert(len(paths) % 2 == 0)
+
+        rail_estimate = which('rail-estimate')
+        if not rail_estimate:
+            raise RuntimeError('Program rail-estimate not found.')
+
+        for input, output in pairwise(paths):
+            with now('create output directory id=%d' % procid):
+                makedirs(dirname(output), exist_ok=True)
+
+            with now('run rail-estimate %s %s id=%d' % (input, output, procid)):
+                run([rail_estimate, '--bins=301', input, output], check=True)
+
+if __name__ == '__main__': main()
