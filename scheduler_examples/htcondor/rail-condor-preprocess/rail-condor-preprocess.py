2369239e3341d7b44c2f0d2af6437dd78b082f5b
Henrique Dante de Almeida <henrique.almeida@linea.org.br>
2023-04-27 13:36:17 -0300

    Add condor scripts to preprocess telescope input data
    The condor scripts are used to call rail-preprocess-parquet in the
    cluster. They partition the input file list into similar sized groups
    and dispatch 1 job per slot, executing the preprocessing step on a set
    of files each, generating a set of preprocessed files (typically more
    files of smaller sizes).
    
    Submitting the condor script requires setting some condor macros:
    
     - input_dir: directory with the input files
     - output_dir: directory for the generated files
     - files: pattern with the input file list
    
    Note: the input directory appears twice, on macros input_dir and
    files.
    
    Pattern expansion supports recursive globbing. Ex:
    
        $ condor_submit rail-condor-preprocess.sub \
            input_dir=input   \
            output_dir=output \
            files="input/**/*.parq"
    
    File generation in the output directory will mirror the directory
    structure from the input directory.

---
+++ scheduler_examples/htcondor/rail-condor-preprocess/rail-condor-preprocess.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from contextlib import contextmanager
+from datetime import datetime
+from os import environ, makedirs
+from os.path import dirname, join
+from socket import gethostname
+from shutil import which
+from subprocess import run
+from sys import argv, stderr
+from time import sleep
+
+PREPROCESS = 'rail-preprocess-parquet'
+
+@contextmanager
+def now(msg):
+    print ('%s: Starting: %s' % (datetime.now(), msg), file=stderr, flush=True)
+    yield
+    print ('%s: Finished: %s' % (datetime.now(), msg), file=stderr, flush=True)
+
+def main():
+    h = gethostname()
+    s = environ.get('_CONDOR_SLOT')
+    procid = int(argv[1])
+
+    with now('rail-condor-preprocess: host=%s slot=%s id=%d' % (h, s, procid)):
+        with now('sleep for 10 seconds (condor warm-up)'):
+            sleep(10)
+
+        input_dir = argv[2]
+        output_base_dir = argv[3]
+        files = argv[4:]
+
+        preprocess = which(PREPROCESS)
+
+        if not preprocess:
+            raise RuntimeError('Program %s not found.' % PREPROCESS)
+
+        for file in files:
+            output_dir = dirname(join(output_base_dir, file))
+            file_path = join(input_dir, file)
+
+            with now('create output directory id=%d' % procid):
+                makedirs(output_dir, exist_ok=True)
+
+            f = file_path
+            o = output_dir
+
+            with now('run %s %s %s id=%d' % (PREPROCESS, f, o, procid)):
+                run([preprocess, '--rows=200000', f, o], check=True)
+
+if __name__ == '__main__': main()
