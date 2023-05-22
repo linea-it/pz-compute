2baaf65bf6c9ac3fd1faeff90a94cadd7c8a1fc9
Henrique Dante de Almeida <henrique.almeida@linea.org.br>
2023-05-22 18:39:24 -0300

    Limit memory and CPU usage on rail-condor
    The RAIL framework uses many auxiliary libraries and some of them, in
    particular numerical computing and file I/O libraries may spawn a large
    number of auxiliary threads and processes. When running on the cluster,
    this is undesirable because each estimation process runs on a single
    cluster thread and auxiliary threads will not help speeding up I/O or
    computation, while having the collateral effect of increasing memory
    usage. This patch limits both memory and CPU usage to the running
    processes to moderate values, so that memory is preserved. The patch
    adds the following configurations:
    
    - Limits maximum virtual memory with setrlimit() (currently 16 GiB)
    - Limits the maximum CPUs used with sched_setaffinity (tries to limit
      usage to 2 Hyperthread siblings and 2 cores sharing the same cache)
    - Sets the OMP_NUM_THREADS environment variable (currenly 3)
    - Sets the ARROW_IO_THREADS environment variable (currently 3)
    - Sets the default Arrow memory pool backend to "system" (malloc())
    
    This patch fixes the memory explosion seen in large tests in the Apollo
    cluster.

--- scheduler_examples/htcondor/rail-condor/rail-condor.py
+++ scheduler_examples/htcondor/rail-condor/rail-condor.py
@@ -2,12 +2,16 @@
 
 from contextlib import contextmanager
 from datetime import datetime
-from os import environ, makedirs
+from os import cpu_count, environ, makedirs, sched_setaffinity
 from os.path import dirname
+from resource import RLIM_INFINITY, RLIMIT_AS, getrlimit, setrlimit
 from shutil import which
 from subprocess import run
 from sys import argv, stderr
 from time import sleep
+
+MAX_VIRTUAL_MEM = 16<<30
+MAX_CPUS = 3
 
 RAIL_ESTIMATE = 'rail-estimate'
 
@@ -21,8 +25,46 @@ def pairwise(iterable):
     a = iter(iterable)
     return zip(a, a)
 
+def limit_memory(size):
+    limits = list(getrlimit(RLIMIT_AS))
+
+    if limits[0] == RLIM_INFINITY:
+        limits[0] = size
+    elif limits[0] < size:
+        return
+
+    limits[0] = size
+
+    # Note: RLIMIT_RSS is not supported on Linux, use RLIMIT_AS as best effort.
+    setrlimit(RLIMIT_AS, limits)
+
+def limit_cpus(start):
+    # Best effort attempt to match Hyperthreading siblings and cache-sharing
+    # cores.
+    total_cpus = cpu_count()
+    neigh1 = start//2*2
+    neigh2 = start//2*2 + 1
+    hyper1 = (neigh1 + total_cpus//2) % total_cpus
+    hyper2 = (neigh2 + total_cpus//2) % total_cpus
+
+    try:
+        sched_setaffinity(0, {neigh1, neigh2, hyper1, hyper2})
+    except OSError:
+        print('Warning: unable to set CPU affinity.', file=stderr, flush=True)
+
 def main():
-    with now('rail-condor: slot=%s' % environ.get('_CONDOR_SLOT')):
+    slot = environ.get('_CONDOR_SLOT')
+    with now('rail-condor: slot=%s' % slot):
+
+        # Condor is not currently configured to limit maximum memory or CPUs, so
+        # limit explicitly so that the libraries don't try to spawn a large
+        # amount of helper threads and processes.
+        limit_memory(MAX_VIRTUAL_MEM)
+        limit_cpus(int(slot.lstrip('slot')))
+        environ['OMP_NUM_THREADS'] = str(MAX_CPUS)
+        environ['ARROW_IO_THREADS'] = str(MAX_CPUS)
+        environ['ARROW_DEFAULT_MEMORY_POOL'] = 'system'
+
         with now('sleep for 30 seconds (condor warm-up)'):
             sleep(30)
 
