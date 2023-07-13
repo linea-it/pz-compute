d848f9d467076d3c1fe04815ed19c1e4c200483c
julia <julia@linea.gov.br>
2023-07-13 18:01:33 -0300

    revert changes

--- scheduler_examples/htcondor/rail-condor/rail-condor.py
+++ scheduler_examples/htcondor/rail-condor/rail-condor.py
@@ -9,8 +9,6 @@ from shutil import which
 from subprocess import run
 from sys import argv, stderr
 from time import sleep
-import yaml
-import glob
 
 MAX_VIRTUAL_MEM = 16<<30
 MAX_CPUS = 3
@@ -55,9 +53,6 @@ def limit_cpus(start):
         print('Warning: unable to set CPU affinity.', file=stderr, flush=True)
 
 def main():
-    start_time = datetime.now()
-    
-    """
     slot = environ.get('_CONDOR_SLOT')
     with now('rail-condor: slot=%s' % slot):
 
@@ -90,37 +85,5 @@ def main():
             with now('run %s %s %s id=%d' % (RAIL_ESTIMATE, input, output, procid)):
                 run([rail_estimate, '--bins=301', '--algorithm=%s' % algorithm,
                     input, output], check=True)
-    """          
-    end_time = datetime.now()
-    duration = end_time - start_time
-    
-    if len(glob.glob('process_info.yaml')) > 0:    
-        
-        # Open the file in append & read mode ('a+')
-        with open('process_info.yaml', "r") as old_file_object:
-            #contents = yaml.safe_load_all(file_object)
-            contents = yaml.load(old_file_object, Loader=yaml.loader.SafeLoader)
-            
-            contents['start_time'] = start_time
-            contents['end_time'] = end_time
-            contents['duration'] = duration
-        with open('process_info.yaml', "w") as new_file_object:
-            yaml.dump(contents, new_file_object)
 
-                            
-            
-            #file_object.write(f'start_time:{start_time}')
-            #file_object.write("\n")
-            #file_object.write(f'end_time:{end_time}')
-            #file_object.write("\n")
-            #file_object.write(f'duration:{duration}')
-            #file_object.write("\n")
-            # TBD: 
-            # get configs from submission file 
-            # get individual machines times from performance script that reads log file
-            # print stats on screen
-            # write stats in process info file
-            # send notification email 
-            
-            
 if __name__ == '__main__': main()
