432376e450b2ef319e0e4a1f8eca1dce9257eff6
luigilcsilva <luigi.lcsilva@gmail.com>
2024-08-09 20:39:07 +0000

    Changing the hard coded paths for flexible ones in the scripts.

--- docs/notebooks/DP02_step_1_random_training_set.py
+++ docs/notebooks/DP02_step_1_random_training_set.py
@@ -1,3 +1,4 @@
+import os
 import dask
 from dask import dataframe as dd
 from dask import delayed
@@ -9,17 +10,31 @@ import glob
 import psutil
 import time
 import tables_io
-import os 
+import getpass
+
+# Identificar o path do usuário
+user = getpass.getuser()
+base_path = f'/lustre/t0/scratch/users/{user}/random_from_files'
+
+# Criar pastas 'output' e 'logs' se não existirem
+output_dir = os.path.join(base_path, 'output')
+logs_dir = os.path.join(base_path, 'logs')
+os.makedirs(output_dir, exist_ok=True)
+os.makedirs(logs_dir, exist_ok=True)
 
 # Configuração do SLURMCluster para usar 12 nós com 56 núcleos lógicos e 128GB de RAM cada
 cluster = SLURMCluster(
     interface="ib0",    # Interface do Lustre
     queue='cpu_small',  # Substitua pelo nome da sua fila
     cores=56,           # Número de núcleos lógicos por nó
-    processes=56,       # Número de processos por nó (um processo por núcleo)
-    memory='128GB',     # Memória por nó
+    processes=28,       # Número de processos por nó (um processo por núcleo)
+    memory='100GB',     # Memória por nó
     walltime='01:00:00',  # Tempo máximo de execução
-    job_extra_directives=['--propagate'],  # Argumentos adicionais para o SLURM
+    job_extra_directives=[
+        '--propagate',
+        f'--output={output_dir}/dask_job_%j.out',  # Redireciona a saída para a pasta output
+        f'--error={output_dir}/dask_job_%j.err'    # Redireciona o erro para a pasta output
+    ],
 )
 
 # Escalando o cluster para usar 6 nós
@@ -28,19 +43,13 @@ cluster.scale(jobs=6)  # Defina para usar 6 nós
 # Definindo o client do Dask
 client = Client(cluster)  
 
-# Medir o tempo de execução - START
-start_wall_time = time.time()
-start_cpu_time = psutil.cpu_times()
-
 # Definição das variáveis pelo usuário
 band_for_cut = 'i' 
-#mag_cut = 24.1  
 mag_cut = 25.2  
 fraction = 0.002  
-process_dir =  os.getcwd()  
 
 # Caminho para o relatório de desempenho do Dask
-performance_report_path = f'{process_dir}/performance_report_{band_for_cut}_{mag_cut}_{fraction}.html'
+performance_report_path = os.path.join(output_dir, f'performance_report_{band_for_cut}_{mag_cut}_{fraction}.html')
 
 with performance_report(filename=performance_report_path):
     # Obter lista de arquivos HDF5 na pasta
@@ -64,9 +73,9 @@ with performance_report(filename=performance_report_path):
     ddf_sampled = ddf_filtered.sample(frac=fraction)
 
     # Caminhos de saída
-    output_path_parquet = f'{process_dir}/output/random_sample_{band_for_cut}_{mag_cut}_{fraction}.parquet'
-    output_path_csv = f'{process_dir}/output/random_sample_{band_for_cut}_{mag_cut}_{fraction}.csv'
-    output_path_hdf5 = f'{process_dir}/output/random_sample_{band_for_cut}_{mag_cut}_{fraction}.hdf5'
+    output_path_parquet = os.path.join(output_dir, f'random_sample_{band_for_cut}_{mag_cut}_{fraction}.parquet')
+    output_path_csv = os.path.join(output_dir, f'random_sample_{band_for_cut}_{mag_cut}_{fraction}.csv')
+    output_path_hdf5 = os.path.join(output_dir, f'random_sample_{band_for_cut}_{mag_cut}_{fraction}.hdf5')
 
     # Computando o dataframe.
     ddf_computed = ddf_sampled.compute()
@@ -80,22 +89,6 @@ with performance_report(filename=performance_report_path):
 
     # Salvar os dados em HDF5
     tables_io.write(ddf_computed, output_path_hdf5)
-
-# Medir o tempo de execução - END
-end_wall_time = time.time()
-end_cpu_time = psutil.cpu_times()
-
-# Calcular o tempo de execução
-total_wall_time = end_wall_time - start_wall_time
-total_cpu_time_user = end_cpu_time.user - start_cpu_time.user
-total_cpu_time_system = end_cpu_time.system - start_cpu_time.system
-
-# Salvar os tempos de execução em um arquivo de texto
-output_time_path = f'{process_dir}/execution_times_{band_for_cut}_{mag_cut}_{fraction}.txt'
-with open(output_time_path, 'w') as f:
-    f.write(f'Total Wall Time: {total_wall_time} seconds\n')
-    f.write(f'Total CPU Time (User): {total_cpu_time_user} seconds\n')
-    f.write(f'Total CPU Time (System): {total_cpu_time_system} seconds\n')
 
 # Fechando o client
 client.close()
