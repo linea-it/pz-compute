a7165f2e0d59897d011bc2648fdc2a1c9f896125
Henrique Dante de Almeida <henrique.almeida@linea.org.br>
2024-08-16 18:11:48 -0300

    Use different default slurm parameters for lephare
    The lephare estimator uses more memory than LineA's limit of 2 GB per
    hart. lephare is a multithreaded algorithm, though, and may scale to a
    large number of harts. This patch changes lephare's default allocation
    to 2 harts per process, which is enough to reduce memory usage to less
    than 60% and is also able to keep an almost continuous 100% CPU usage.
    The chosen default parameters are:
    
    	-N26 -n1016 -c2
    
    The parameters mean 26 machines, 1016 harts (half of LineA's total
    harts) and 2 harts per process.

--- scheduler_scripts/slurm/pz-compute.py
+++ scheduler_scripts/slurm/pz-compute.py
@@ -11,6 +11,7 @@ from yaml import safe_load
 
 SBATCH_ARGS = '-N 26 -n 2032'
 SBATCH_ARGS_TPZ = '-N 26 -n 1316 --mem-per-cpu=3500M'
+SBATCH_ARGS_LEPHARE = '-N 26 -n 1016 -c2'
 
 @dataclass
 class Configuration:
@@ -47,8 +48,13 @@ def load_configuration(conffile):
     if tmp:
         config = replace(config, **tmp)
 
-        if tmp.get('algorithm') == 'tpz' and not 'sbatch_args' in tmp:
-            config.sbatch_args = split(SBATCH_ARGS_TPZ)
+        if not 'sbatch_args' in tmp:
+            algorithm = tmp.get('algorithm')
+
+            if algorithm == 'tpz':
+                config.sbatch_args = SBATCH_ARGS_TPZ
+            elif algorithm == 'lephare':
+                config.sbatch_args = SBATCH_ARGS_LEPHARE
 
     config.inputdir = to_path(config.inputdir)
     config.outputdir = to_path(config.outputdir)
