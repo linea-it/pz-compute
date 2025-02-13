56733e3eac852bb25a70dae362ce190b249a6349
HeloisaMengisztki <heloisasmengisztki@gmail.com>
2025-02-13 14:26:26 -0300

    some refactoring

--- ondemand/ondemand_utils/ondemand_utils/run_setup.py
+++ ondemand/ondemand_utils/ondemand_utils/run_setup.py
@@ -1,29 +1,7 @@
 #!/usr/bin/env python3
 
-'''Prepare directories and ancillary files to run 
-   rail-estimate script in parallel using Slurm. 
-   Write provenance info in text file. 
-
-   Usage:
-   
-   prepare-pz-test [options] [--] <process_id> <comment> 
-   prepare-pz-test -h | --help
-   prepare-pz-test --version
-   
-   Options:
-   -h --help   Show help text.
-   --version   Show version.
-   -a <algorithm>, --algorithm=<algorithm> fzboost, bpz, gpz, tpz.
-   -p <process id>, --process_id=<process id> Integer or short string without 
-                                               blank spaces to identify the 
-                                               process. There is no warranty of
-                                               uniqueness, only if in the same 
-                                               parent directory. If empty, 
-                                               attribute test_pz_compute_x where
-                                               x is incremental integer.
-   -c <comment>, --comment=<comment> Long string (enclosed in quotes) with the 
-                                     process description. (optional) 
-                                      
+'''
+Uses pz_run_setup.py to create processes diretories.
 '''
 
 import os
@@ -35,194 +13,17 @@ import yaml
 import getpass
 from datetime import datetime
 
+import importlib.util
+import sys
 
-ENV = None
-SCRATCH = os.environ.get('SCRATCH')
+path = "../scheduler_examples/slurm/setup/pz_run_setup.py"
 
-APP_PZ_COMPUTE_PATH = '/lustre/t0/scratch/users/app.photoz/pz-compute'
-LSST_DP02 = '/lustre/t1/cl/lsst/dp02/secondary/catalogs/skinny/hdf5/'
+spec = importlib.util.spec_from_file_location("file", path)
+pz_run_setup = importlib.util.module_from_spec(spec)
+sys.modules["file"] = pz_run_setup
+spec.loader.exec_module(pz_run_setup)
 
-class SetupDir:
-    def __init__(self, process_id, comment, algorithm, will_train, creation_path, use_all_dp0_dataset):
-        self.process_id = process_id #"Integer or short string without blank spaces"
-        self.comment = comment       #"Comment with process description (enclosed in quotes)"
-        self.algorithm = algorithm   #"Specify the algorithm to run"
-        self.will_train = will_train
-        self.user = getpass.getuser()
-        self.creation_path = creation_path
-        self.use_all_dp0_dataset = use_all_dp0_dataset
-
-
-def set_process_id(process_id, algorithm, creation_path):
-    if process_id:
-        duplicate_id = os.path.isdir(os.path.join(creation_path, process_id))
-        if duplicate_id:
-            raise Exception(f'Process {process_id} already exists!\nAborting...')
-    else: 
-        old_process_ids = glob.glob(f'{creation_path}/test_pz_compute_{algorithm}*') 
-        if len(old_process_ids)>0:
-            old_n = []
-            for old_process_id in old_process_ids:
-                old_n.append(int(old_process_id.split('_')[-1]))
-            max_id = max(old_n)
-            process_id = f'test_pz_compute_{algorithm}_'+ str(max_id+1)
-        else:
-            process_id = f'test_pz_compute_{algorithm}_0'
-    
-    return process_id
-        
-def define_setup_dir(process_id, comment, algorithm, will_train, creation_path, use_all_dp0_dataset, env):
-    global ENV
-    ENV = env
-    
-    print(ENV, SCRATCH)
-    
-    if algorithm == None:
-        raise Exception("Algorithm must be informed, options are: fzboost, bpz, tpz, gpz")
-       
-    if will_train == False:
-        print("Train run not defined, remember to add the .pkl file")
-        
-    if creation_path is None:
-        print("Creation path not defined, setting it to your scratch")
-        creation_path = SCRATCH
-    
-    process_id = set_process_id(process_id, algorithm, creation_path)
-    
-    return SetupDir(process_id, comment, algorithm, will_train, creation_path, use_all_dp0_dataset)
-
-
-def represent_list(dumper, data):
-    return dumper.represent_sequence('tag:yaml.org,2002:seq', data, flow_style=True)
-
-def print_output(args, yaml_file):
-    print('---------------------------------------')
-    print(f'Process ID: {args.process_id}')
-    if not args.comment:
-        args.comment = ' --- ' 
-    print(f'User: {args.user}')
-    print(f'Algorithm: {args.algorithm}')
-    print(f'Description: {args.comment}')
-    print('---------------------------------------')
-    print()
-    
-    print(f'Process info saved in {yaml_file}')    
-    print()
-
-def create_yaml_process_file(args):
-    yaml_file = f'{args.creation_path}/{args.process_id}/process_info.yaml'
-
-    with open(yaml_file, 'w') as outfile:
-        yaml.dump(vars(args), outfile, default_flow_style=False)
-    
-    return yaml_file
-
-def create_yaml_pz_compute_train(args):
-    yaml_file_config = f'{args.creation_path}/{args.process_id}/pz-train.yaml'
-    
-    yaml.add_representer(list, represent_list)
-    
-    configs_train = {'algorithm': args.algorithm, 'sbatch_args': ["-N1", "-n1"], 'param_file':f"{args.algorithm}_train.yaml", 'inputfile':"train-file.hdf5"}
-    
-    with open(yaml_file_config, 'w') as outfile:
-        yaml.dump(configs_train, outfile, default_flow_style=False)
-
-def create_yaml_pz_compute(args):
-    yaml_file_config = f'{args.creation_path}/{args.process_id}/pz-compute.yaml'
-    
-    yaml.add_representer(list, represent_list)
-    
-    configs_pz_compute = {'algorithm': args.algorithm, 'sbatch_args': ["-N5", "-n140"], 'param_file':f"{args.algorithm}_estimate.yaml", 'calib_file':f"estimator_{args.algorithm}.pkl"}
-    
-    with open(yaml_file_config, 'w') as outfile:
-        yaml.dump(configs_pz_compute, outfile, default_flow_style=False)
-
-def create_required_dirs(args):
-    process_dir = f'{args.creation_path}/{args.process_id}' 
-    try:
-        os.makedirs(f'{process_dir}', exist_ok = True)
-        os.makedirs(f'{process_dir}/input', exist_ok = True)
-    except OSError as error:
-        print('Failed to create directories')
-    
-def add_input_data(args):
-    if args.use_all_dp0_dataset:
-        if os.path.exists(f'{args.creation_path}/{args.process_id}/input'):
-            pattern = '*.hdf5'
-            target_dir = f'{args.creation_path}/{args.process_id}/input/'
-            
-            for file_path in glob.glob(os.path.join(LSST_DP02, pattern)):
-                filename = os.path.basename(file_path)
-                target_file = os.path.join(target_dir, filename)
-
-                try:
-                    os.symlink(file_path, target_file)
-                except FileExistsError:
-                    print(f"File {target_file} already exists.")
-                except Exception as e:
-                    print(f"Error creating symbolic link to {file_path}: {e}")
-            
-            print("\nUsing the skinny tables for the complete LSST DP0.2\n")
-        else:
-            print("The input dir does not exists.")
-    else:
-        print("\nNot using the complete LSST DP0.2 dataset.")
-        print(f'Please add manually the data in the input dir with the folowing command:\n ln -s <origin_path>/*.hdf5 {args.process_id}/input/')
-        print()
-
-def copy_configs_file(args):
-    file_name_train = f"{args.algorithm}_train.yaml"
-    file_name_estimate = f"{args.algorithm}_estimate.yaml"
-    dst = f"{args.creation_path}/{args.process_id}/"
-    
-    if ENV == "prod":
-        if args.will_train:
-            src = f'{APP_PZ_COMPUTE_PATH}/doc/algorithms_config/{file_name_train}'
-            shutil.copy(src, dst)
-            create_yaml_pz_compute_train(args)
-
-        src = f'{APP_PZ_COMPUTE_PATH}/doc/algorithms_config/{file_name_estimate}'
-        shutil.copy(src, dst)
-
-    elif ENV == "dev":
-        if args.will_train:
-            src = f'{SCRATCH}/pz-compute/doc/algorithms_config/{file_name_train}'
-            shutil.copy(src, dst)
-            create_yaml_pz_compute_train(args)
-
-        src = f'{SCRATCH}/pz-compute/doc/algorithms_config/{file_name_estimate}'
-        shutil.copy(src, dst)
-    else:
-        print("Env not defined, not creating the configurations yaml")
-        return
-    
-def copy_run_notebook(args):
-    notebook_file_origin = f"2.pz-compute-template.ipynb"
-    notebook_file_dest = f"{args.process_id}.ipynb"
-    dst = f"{args.creation_path}/{args.process_id}/{notebook_file_dest}"
-    
-    if ENV == "prod":
-        src = f'{APP_PZ_COMPUTE_PATH}/ondemand/{notebook_file_origin}'
-        shutil.copy(src, dst)
-
-    elif ENV == "dev":
-        src = f'{SCRATCH}/pz-compute/ondemand/{notebook_file_origin}'
-        shutil.copy(src, dst)
-    else:
-        print("Env not defined, not copying the notebook for the run")
-        return
-    
 def create_run_dir(algorithm=None, process_id=None, comment=None, will_train=False, creation_path=None, use_all_dp0_dataset=False, env="dev"):
-    args = define_setup_dir(process_id, comment, algorithm, will_train, creation_path, use_all_dp0_dataset, env) 
     
-    create_required_dirs(args)
+    pz_run_setup.create_process_dir(algorithm, process_id, comment, will_train, creation_path, use_all_dp0_dataset, env)
     
-    add_input_data(args)
-    
-    create_yaml_pz_compute(args)
-    yaml_file = create_yaml_process_file(args)
-    
-    print_output(args, yaml_file)
-    
-    copy_configs_file(args)
-    copy_run_notebook(args)
